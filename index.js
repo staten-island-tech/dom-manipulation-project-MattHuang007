fca4c1e9fd295604f42ecd3a5e58a52459d23004
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2023-10-25 13:12:29 -0400

    mn

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ DOMselectors ={
     form: document.querySelector("#form"),
     firstname: document.querySelector("firstName"),
 }
+{
+    
+}
 //no "onclick"
+//const Album= makeAlbum()
+//addCard(Album)
+//clearFields()
+//addRemoveButtons
