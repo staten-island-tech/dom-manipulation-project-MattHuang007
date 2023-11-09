2ba753fb6fadbb75ce92469a8ba72b2e1c13c244
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2023-11-08 22:27:49 -0500

    n

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
-const DOMselectors={
-    form: document.querySelector("#form"),
-    firstname: document.querySelector("firstName"),
-    lastname: document.querySelector("lastName"),
+const DOMselectors = {
+  form: document.querySelector("#form"),
+  firstname: document.querySelector(".firstName"),
+  lastname: document.querySelector(".lastName"),
+  image: document.querySelector(".image"),
+};
+function createobject() {
+  const card = {
+    firstname: DOMselectors.firstname.value,
+    image: DOMselectors.image.value,
+    lastname: DOMselectors.lastname.value,
+  };
+  const newCard = createCard(card);
+  const container = document.querySelector(".container");
+  container.appendChild(newCard);
+  clearFields();
 }
-addEventListener()
+function createCard(card) {
+  const newCard = document.createElement("div");
+  newCard.classList.add("card");
+  const firstname = document.createElement("h2");
+  firstname.innerText = card.firstname;
+  firstname.classList.add("displayartist");
+  const lastname = document.createElement("h3");
+  lastname.classList.add("displayalbum");
+  lastname.innerText = card.lastname;
+  const image = document.createElement("img");
+  image.src = card.image;
+  image.classList.add("display-image");
+  const button = document.createElement("button");
+  button.classList.add("button");
+  button.addEventListener("click", deleteParent);
+  button.innerText = "delete";
+  newCard.appendChild(firstname);
+  newCard.appendChild(image);
+  newCard.appendChild(lastname);
+  newCard.appendChild(button);
+  return newCard;
+}
+function clearFields() {
+  DOMselectors.firstname.value = "";
+  DOMselectors.image.value = "";
+  DOMselectors.lastname.value = "";
+}
+function deleteParent(event) {
+  event.target.parentElement.remove();
+}
+// document.querySelector(".submitbutton").addEventListener("click", function(event){
+//     createobject()
+// })
 //no "onclick"
 //const Album= makeAlbum()
 //addCard(Album)
 //clearFields()
-//addRemoveButtons
+//addRemoveButton
 
 /*function.removebutton(){
     let buttons = document.querySelectorAll("button");
